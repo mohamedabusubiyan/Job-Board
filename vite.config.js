68a55a1edfb5d739f25c3ebccfc98455f0cb39a3
Mohamed Abu Subiyan <mohamedabusubiyanm@gmail.com>
2025-09-10 15:41:31 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite'
+import forms from '@tailwindcss/forms';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss({
+            content: [
+                "./resources//*.blade.php",
+                "./resources//*.js",
+                "./resources//*.vue",
+            ],
+            plugins: [forms],
+        }),
+    ],
+});
